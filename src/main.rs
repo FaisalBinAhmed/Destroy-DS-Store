20d33f8d6b6afe56005d9bd42d291b70afa33ceb
Faisal Bin Ahmed <faisalbin@outlook.com>
2022-11-12 19:24:04 +0100

    removing target folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+
+
+use std::path::Display;
+
+use walkdir::WalkDir;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let current_dir: String = String::from("./");
+
+    let mut counter: i32 = 0;
+    let mut ds_path: Vec<String> = vec![];
+
+    for entry in WalkDir::new(&current_dir) {
+    let entry = entry.unwrap();
+
+    if entry.file_name() == ".DS_Store" {
+        counter+=1;
+        ds_path.push(entry.path().display().to_string())
+    }
+
+    println!("{}", entry.path().display());
+}
+
+println!("total DS_Store found: {}", counter);
+println!("Locations: {:?}", ds_path)
+
+
 }
