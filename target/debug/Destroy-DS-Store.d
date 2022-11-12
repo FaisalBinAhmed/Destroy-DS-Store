4346cd3825636eb2308a19063d9de9e7b392e82c
Faisal Bin Ahmed <faisalbin@outlook.com>
2022-11-12 19:24:44 +0100

    removing target folder

--- target/debug/Destroy-DS-Store.d
+++
@@ -1,1 +0,0 @@
-/Users/faisal/Documents/GitHub/Destroy-DS-Store/target/debug/Destroy-DS-Store: /Users/faisal/Documents/GitHub/Destroy-DS-Store/src/main.rs
--- target/debug/deps/Destroy_DS_Store-6388b02ef10e4cc8.d
+++
@@ -1,5 +0,0 @@
-/Users/faisal/Documents/GitHub/Destroy-DS-Store/target/debug/deps/Destroy_DS_Store-6388b02ef10e4cc8: src/main.rs
-
-/Users/faisal/Documents/GitHub/Destroy-DS-Store/target/debug/deps/Destroy_DS_Store-6388b02ef10e4cc8.d: src/main.rs
-
-src/main.rs:
